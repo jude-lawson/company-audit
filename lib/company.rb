094558117118877938f86cfa057774144d94b84d
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 11:52:54 -0600

    WIP create_index for lookups to prep for iteration 3. Completes spec requirements for iteration 2

--- lib/company.rb
+++ lib/company.rb
@@ -17,7 +17,7 @@ class Company
   def load_employees(filename)
     loaded = load('./data/', filename)
     if loaded.class == CSV::Table
-      create_index(loaded, @employees)
+      create_index(loaded, 'employees')
       return { success: true, error: nil}
     else
       return { success: false, error: loaded}
@@ -27,7 +27,7 @@ class Company
   def load_projects(filename)
     loaded = load('./data/', filename)
     if loaded.class == CSV::Table
-      create_index(loaded, @employees)
+      create_index(loaded, 'projects')
       return { success: true, error: nil}
     else
       return { success: false, error: loaded}
@@ -37,14 +37,24 @@ class Company
   def load_timesheets(filename)
     loaded = load('./data/', filename)
     if loaded.class == CSV::Table
-      create_index(loaded, @employees)
+      create_index(loaded, 'timesheets')
       return { success: true, error: nil}
     else
       return { success: false, error: loaded}
     end
   end
   
-    def create_index(data, type)
-      'loaded'
+  def create_index(data, collection)
+    lookup = {}
+    data.each do |row|
+      if collection == 'employees'
+        lookup[row[:id].to_i] = Employee.new(row[:id], row[:name],
+                                        row[:role], row[:start_date],
+                                        row[:end_date])
+      end
     end
+    if collection == 'employees'
+      @employees = lookup
+    end
+  end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -1,9 +1,12 @@
 require './test/test_helper'
 require './lib/company'
+require './lib/employee'
 
 class CompanyTest < Minitest::Test
   def setup
     @company = Company.new
+    @employee1 = Employee.new('1','Susan Smith', 'Manager', '2016-01-01', '2018-02-20')
+    @employee2 = Employee.new('2','John Smith', 'Engineer', '2016-01-01', '2018-02-20')
   end
 
   def test_company_exists
@@ -34,5 +37,11 @@ class CompanyTest < Minitest::Test
     assert_equal expected, @company.load_employees('bad_employees.csv')
     assert_equal expected, @company.load_projects('bad_projects.csv')
     assert_equal expected, @company.load_timesheets('bad_timesheets.csv')
+  end
+
+  def test_loading_in_employee_data_creates_lookup_table
+    @company.load_employees('employees.csv')
+    expected = { 1 => @employee1, 2 => @employee2 }
+    assert_equal expected, @company.employees
   end
 end
