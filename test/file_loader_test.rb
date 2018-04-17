f052a87385729a70b8c686c2ca78795311befc47
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 10:10:05 -0600

    Completes FileLoader test and module

--- test/file_loader_test.rb
+++ test/file_loader_test.rb
@@ -2,20 +2,23 @@ require './test/test_helper'
 require './lib/file_loader'
 
 class FileLoaderTest < Minitest::Test
+  include FileLoader
 
   def setup
     path = './test/test_data/'
     filename = 'file_loader_test_data.csv'
-    @parsed_data = FileLoader.load(path, filename)
-    @thing1 = @parsed_data[1]
-    @thing2 = @parsed_data[2]
+    @parsed_data = load(path, filename)
+    # require 'pry';binding.pry
+    @thing1 = @parsed_data[0]
+    @thing2 = @parsed_data[1]
   end
 
-  def test_parsed_data_is_csv_object
-    assert_instance_of CSV, @parsed_data
+  def test_parsed_data_is_csv_table
+    assert_instance_of CSV::Table, @parsed_data
   end
 
-  def test_parsed_data_returns_correct_elements
-    assert_equal 
+  def test_parsed_data_returns_elements_as_csv_rows
+    assert_instance_of CSV::Row, @thing1
+    assert_instance_of CSV::Row, @thing2
   end
 end
