48da7c5d5c8ed58531bdce06a2007e5cc3a6de0c
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 09:22:35 -0600

    Adds in first tests for employee class

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -1,10 +1,26 @@
 require './test/test_helper'
+require './modules/date_handler'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+  def setup
+    employee_id = '5'
+    name = 'Sally Sparrow'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    @employee = Employee.new(employee_id, name, role, start_date, end_date)
+    @start_date = DateHandler.string_to_date(start_date)
+  end
   def test_instantiation
+    assert_instance_of Employee, @employee
   end
 
   def test_attributes
+    assert_equal Integer, @employee.id.class
+    assert_equal 5, @employee.id
+    assert_equal 'Sally Sparrow', @employee.name
+    assert_equal 'Engineer', @employee.role
+    assert_equal 
   end
 end
