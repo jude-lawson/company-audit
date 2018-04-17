52af2bb1226ffb0c6e912d33a39819ae65211950
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 09:27:52 -0600

    Completes employee test and adds employee attributes

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,16 @@
+require './modules/date_handler'
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(id, name, role, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @role = role
+    @start_date = DateHandler.string_to_date(start_date)
+    @end_date = DateHandler.string_to_date(end_date)
+  end
 end
