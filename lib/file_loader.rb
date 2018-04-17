e6e538465bd2f0faf72e766fa1b145d40feb5a74
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 10:59:40 -0600

    Working on file loader

--- lib/file_loader.rb
+++ lib/file_loader.rb
@@ -2,6 +2,20 @@ require 'csv'
 
 module FileLoader
   def load(path, filename)
-    CSV.read("#{path}#{filename}", headers: :true, header_converters: :symbol)
-  end    
+    data = CSV.read("#{path}#{filename}", headers: :true, header_converters: :symbol)
+    check_for_nil(data)
+  end
+  
+  def check_for_nil(data)
+    data.each do |row|
+      h = row.to_hash
+      values = h.values.all?
+      fields = h.keys.all?
+      if values && fields
+        data
+      else
+        'bad_data'
+      end 
+    end
+  end
 end
--- test/file_loader_test.rb
+++ test/file_loader_test.rb
@@ -5,20 +5,30 @@ class FileLoaderTest < Minitest::Test
   include FileLoader
 
   def setup
-    path = './test/test_data/'
-    filename = 'file_loader_test_data.csv'
-    @parsed_data = load(path, filename)
-    # require 'pry';binding.pry
-    @thing1 = @parsed_data[0]
-    @thing2 = @parsed_data[1]
+    # path = './test/test_data/'
+    # filename = 'file_loader_test_data.csv'
+    # @parsed_data = load(path, filename)
+    # @thing1 = @parsed_data[0]
+    # @thing2 = @parsed_data[1]
   end
 
   def test_parsed_data_is_csv_table
+    skip
     assert_instance_of CSV::Table, @parsed_data
   end
 
   def test_parsed_data_returns_elements_as_csv_rows
+    skip
     assert_instance_of CSV::Row, @thing1
     assert_instance_of CSV::Row, @thing2
+  end
+
+  def test_catching_bad_data
+    error1 = load('./data/', 'bad_employees.csv')
+    error2 = load('./data/', 'bad_projects.csv')
+    error3 = load('./data/', 'bad_timesheets.csv')
+    assert_equal 'bad_data', error1
+    assert_equal 'bad_data', error2
+    assert_equal 'bad_data', error3
   end
 end
