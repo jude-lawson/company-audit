bc457878cd41178bb1df474753d5e467c93691f2
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-17 11:22:52 -0600

    adds in load_employees, load_projects, and load_timesheets tests and methods

--- lib/company.rb
+++ lib/company.rb
@@ -1,4 +1,9 @@
+require './lib/file_loader'
+require 'csv'
+
 class Company
+  include FileLoader
+
   attr_reader :employees,
               :projects,
               :timesheets
@@ -8,4 +13,38 @@ class Company
     @projects = []
     @timesheets = []
   end
+
+  def load_employees(filename)
+    loaded = load('./data/', filename)
+    if loaded.class == CSV::Table
+      create_index(loaded, @employees)
+      return { success: true, error: nil}
+    else
+      return { success: false, error: loaded}
+    end
+  end
+
+  def load_projects(filename)
+    loaded = load('./data/', filename)
+    if loaded.class == CSV::Table
+      create_index(loaded, @employees)
+      return { success: true, error: nil}
+    else
+      return { success: false, error: loaded}
+    end
+  end
+
+  def load_timesheets(filename)
+    loaded = load('./data/', filename)
+    if loaded.class == CSV::Table
+      create_index(loaded, @employees)
+      return { success: true, error: nil}
+    else
+      return { success: false, error: loaded}
+    end
+  end
+  
+    def create_index(data, type)
+      'loaded'
+    end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -21,4 +21,18 @@ class CompanyTest < Minitest::Test
   def test_company_begins_with_no_timesheets
     assert_equal [], @company.timesheets
   end
+
+  def test_load_good_data_returns_correct_message
+    expected = {success: true, error: nil}
+    assert_equal expected, @company.load_employees('employees.csv')
+    assert_equal expected, @company.load_projects('projects.csv')
+    assert_equal expected, @company.load_timesheets('timesheets.csv')
+  end
+
+  def test_load_bad_data_returns_error
+    expected = {success: false, error: 'bad_data'}
+    assert_equal expected, @company.load_employees('bad_employees.csv')
+    assert_equal expected, @company.load_projects('bad_projects.csv')
+    assert_equal expected, @company.load_timesheets('bad_timesheets.csv')
+  end
 end
